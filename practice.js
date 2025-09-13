6cd61efef5cfdbf5783bc6a295a5dabc783279bd
Sirumba <simonsmaina45@gmail.com>
2025-09-13 21:12:06 +0300

    created 0-javascript_is_amazing.js

---
+++ practice.js
@@ -0,0 +1,19 @@
+const myName = "Sule"
+const age = 30
+const lovesCoding = true
+console.log(`Hi,my name is ${myName}, I am ${age} years old and it is ${lovesCoding} that I love coding.`)
+
+const fullName = "Suleiman"
+const age2 = 30
+const skills = ["HTML", "CSS", "JavaScript", "React", "Node", "Python", "Django"]
+console.log(`My name is ${fullName}. I am ${age2} years old. My skills are ${skills.join(", ")}.`)
+const IsAvailableForWork = true
+
+console.log(
+    `Developer Profile:
+Name: ${fullName}
+Age: ${age2}
+Skills: ${skills.join(", ")}
+Available for work: ${IsAvailableForWork}
+`
+)
